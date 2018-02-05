d427a4e3b6a5f8749a4632d58227f61f1f065593
ZhouQi <bitwolaiye@gmail.com>
2018-02-06 00:50:23 +0800

    build script update

--- build-mac.sh
+++ build-mac.sh
@@ -1,1 +1,12 @@
+rm -rf build
+rm -rf dist
+
 ~/miniconda2/envs/electrumq/bin/python setup-release.py py2app
+
+version=pre0.0.2
+
+cd dist
+tar -czf electrumq-mac-$version.tar.gz electrumq.app
+rm ~/work/electrumq.org/_site/download/electrumq-mac-$version.tar.gz
+mv electrumq-mac-$version.tar.gz ~/work/electrumq.org/_site/download/
+cd ..
--- build-win.sh
+++ build-win.sh
@@ -1,3 +1,6 @@
+rm -rf build
+rm -rf dist
+
 ## These settings probably don't need change
 export WINEPREFIX=/opt/wine64
 #export WINEARCH='win32'
@@ -8,7 +11,9 @@ PYTHON="wine $PYHOME/python.exe -OO -B"
 $PYTHON "C:/pyinstaller/pyinstaller.py" electrumq.spec
 wine "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" /DPRODUCT_VERSION=pre0.0.2 electrumq.nsi
 
-rm ~/work/electrumq.org/_site/download/electrumq-win-pre0.0.2.exe
-mv dist/electrumq-setup.exe ~/work/electrumq.org/_site/download/electrumq-win-pre0.0.2.exe
+version=pre0.0.2
+
+rm ~/work/electrumq.org/_site/download/electrumq-win-$version.exe
+mv dist/electrumq-setup.exe ~/work/electrumq.org/_site/download/electrumq-win-$version.exe
 
 
